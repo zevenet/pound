eb471de8f26e0367dd08d299d2252fa8b2b958a9
Emilio <emilio.campos@zevenet.com>
2017-07-17 09:41:32 +0200

    [Improvement] Added support to compile pound with openssl 1.0.2
    Signed-off-by: Emilio <emilio.campos@zevenet.com>
    
    	new file:   dh2048.h
    	modified:   svc.c

--- svc.c
+++ svc.c
@@ -1512,6 +1512,39 @@ do_RSAgen(void)
     return;
 }
 
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000
+static inline int DH_set0_pqg(DH *dh, BIGNUM *p, BIGNUM *q, BIGNUM *g)
+{
+	/* If the fields p and g in d are NULL, the corresponding input
+	 * parameters MUST be non-NULL.  q may remain NULL.
+	 */
+	if ((dh->p == NULL && p == NULL)
+	    || (dh->g == NULL && g == NULL))
+		return 0;
+
+	if (p != NULL) {
+		BN_free(dh->p);
+		dh->p = p;
+	}
+	if (q != NULL) {
+		BN_free(dh->q);
+		dh->q = q;
+	}
+	if (g != NULL) {
+		BN_free(dh->g);
+		dh->g = g;
+	}
+
+	if (q != NULL) {
+		dh->length = BN_num_bits(q);
+	}
+
+	return 1;
+}
+#endif
+
+
 #include    "dh512.h"
 
 #if DH_LEN == 1024
