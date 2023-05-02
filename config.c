2f7c6e3ef42ce0e135151ba0b17ceaf6c71487d2
nevola <laura.garcia@zevenet.com>
2023-05-02 13:46:09 +0200

    config: remove include and includedir config directives
    These directives are not being used.
    
    Signed-off-by: nevola <laura.garcia@zevenet.com>

--- config.c
+++ config.c
@@ -88,14 +88,12 @@ static regex_t Service, ServiceName, URL, OrURLs, HeadRequire, HeadDeny,
 static regex_t Redirect, TimeOut, WSTimeOut, Session, Type, TTL, ID, DynScale;
 static regex_t ClientCert, AddHeader, AddResponseHeader, DisableProto,
   SSLAllowClientRenegotiation, SSLHonorCipherOrder, Ciphers;
-static regex_t CAlist, VerifyList, CRLlist, NoHTTPS11, Grace, Include, ConnTO,
+static regex_t CAlist, VerifyList, CRLlist, NoHTTPS11, Grace, ConnTO,
   IgnoreCase, Ignore100continue, HTTPS;
 static regex_t Disabled, Threads, CNName, Anonymise, DHParams, ECDHCurve;
 
 static regex_t ControlGroup, ControlUser, ControlMode;
 static regex_t ThreadModel;
-
-static regex_t IncludeDir;
 
 static regex_t ForceHTTP10, SSLUncleanShutdown;
 
@@ -160,70 +158,6 @@ void conf_err(const char *msg)
   exit(1);
 }
 
-static void include_dir(const char *conf_path)
-{
-  DIR *dp;
-  struct dirent *de;
-
-  char buf[512];
-  char *files[200], *cp;
-  int filecnt = 0;
-  int idx, use;
-
-  logmsg(LOG_DEBUG, "Including Dir %s", conf_path);
-
-  if ((dp = opendir(conf_path)) == NULL) {
-    conf_err("can't open IncludeDir directory");
-    exit(1);
-  }
-
-  while ((de = readdir(dp)) != NULL) {
-    if (de->d_name[0] == '.')
-      continue;
-    if ((strlen(de->d_name) >= 5
-         && !strncmp(de->d_name + strlen(de->d_name) - 4, ".cfg", 4))
-        || (strlen(de->d_name) >= 6
-            && !strncmp(de->d_name + strlen(de->d_name) - 5, ".conf", 5))
-      ) {
-      snprintf(buf, sizeof(buf), "%s%s%s", conf_path,
-               (conf_path[strlen(conf_path) - 1] == '/') ? "" : "/",
-               de->d_name);
-      buf[sizeof(buf) - 1] = 0;
-      if (filecnt == sizeof(files) / sizeof(*files)) {
-        conf_err("Max config files per directory reached");
-      }
-      if ((files[filecnt++] = strdup(buf)) == NULL) {
-        conf_err("IncludeDir out of memory");
-      }
-      continue;
-    }
-  }
-  /* We order the list, and include in reverse order, because include_file adds to the top of the list */
-  while (filecnt) {
-    use = 0;
-    for (idx = 1; idx < filecnt; idx++)
-      if (strcmp(files[use], files[idx]) < 0)
-        use = idx;
-
-    logmsg(LOG_DEBUG, " I==> %s", files[use]);
-
-    // Copied from Include logic
-    if (cur_fin == (MAX_FIN - 1))
-      conf_err("Include nesting too deep");
-    cur_fin++;
-    f_name[cur_fin] = files[use];
-    if ((f_in[cur_fin] = fopen(files[use], "rt")) == NULL) {
-      logmsg(LOG_ERR, "%s line %d: Can't open included file %s",
-             f_name[cur_fin], n_lin[cur_fin], files[use]);
-      exit(1);
-    }
-    n_lin[cur_fin] = 0;
-    files[use] = files[--filecnt];
-  }
-
-  closedir(dp);
-}
-
 static char *conf_fgets(char *buf, const int max)
 {
   int i;
@@ -248,23 +182,6 @@ static char *conf_fgets(char *buf, const int max)
         || !regexec(&Comment, buf, 4, matches, 0))
       /* comment or empty line */
       continue;
-    if (!regexec(&Include, buf, 4, matches, 0)) {
-      buf[matches[1].rm_eo] = '\0';
-      if (cur_fin == (MAX_FIN - 1))
-        conf_err("Include nesting too deep");
-      cur_fin++;
-      if ((f_name[cur_fin] = strdup(&buf[matches[1].rm_so])) == NULL)
-        conf_err("Include out of memory");
-      if ((f_in[cur_fin] = fopen(&buf[matches[1].rm_so], "rt")) == NULL)
-        conf_err("can't open included file");
-      n_lin[cur_fin] = 0;
-      continue;
-    }
-    if (!regexec(&IncludeDir, buf, 4, matches, 0)) {
-      buf[matches[1].rm_eo] = '\0';
-      include_dir(buf + matches[1].rm_so);
-      continue;
-    }
     return buf;
   }
 }
@@ -1687,7 +1604,7 @@ static void load_certdir(int has_other, LISTENER * res, const char *dir_path)
   }
 
   if ((dp = opendir(dir_path)) == NULL) {
-    conf_err("can't open IncludeDir directory");
+    conf_err("can't open Cert Dir directory");
     exit(1);
   }
 
@@ -2226,10 +2143,6 @@ void config_parse(const int argc, char **const argv)
       || regcomp(&SSLUncleanShutdown,
                  "^[ \t]*SSLUncleanShutdown[ \t]+\"(.+)\"[ \t]*$",
                  REG_ICASE | REG_NEWLINE | REG_EXTENDED)
-      || regcomp(&Include, "^[ \t]*Include[ \t]+\"(.+)\"[ \t]*$",
-                 REG_ICASE | REG_NEWLINE | REG_EXTENDED)
-      || regcomp(&IncludeDir, "^[ \t]*IncludeDir[ \t]+\"(.+)\"[ \t]*$",
-                 REG_ICASE | REG_NEWLINE | REG_EXTENDED)
       || regcomp(&ConnTO, "^[ \t]*ConnTO[ \t]+([1-9][0-9]*)[ \t]*$",
                  REG_ICASE | REG_NEWLINE | REG_EXTENDED)
       || regcomp(&IgnoreCase, "^[ \t]*IgnoreCase[ \t]+([01])[ \t]*$",
@@ -2483,8 +2396,6 @@ void config_parse(const int argc, char **const argv)
   regfree(&NoHTTPS11);
   regfree(&ForceHTTP10);
   regfree(&SSLUncleanShutdown);
-  regfree(&Include);
-  regfree(&IncludeDir);
   regfree(&ConnTO);
   regfree(&IgnoreCase);
   regfree(&Ignore100continue);
